dfad770d7fdec542eef41cc075129b9b6aec7499
Paloma MOUHALI <pmouhali@e1r7p14.42.fr>
2020-01-29 18:18:00 +0100

    ft_strcmp

---
+++ ft_strcmp.s
@@ -0,0 +1,22 @@
+	global _ft_strcmp
+	section .text
+
+_ft_strcmp:
+	xor rax, rax
+	mov r8d, 1
+	mov r9d, -1
+
+loop:
+	cmp dil, sil
+	cmovg eax, r8d
+	cmovl eax, r9d
+
+	cmp dil, 0 
+	je end
+
+	inc rdi
+	inc rsi
+	jmp loop
+
+end:
+	ret
--- main.c
+++ main.c
@@ -2,11 +2,20 @@
 #include <inttypes.h>
 
 int	ft_strlen(char*);
+int	ft_strcmp(char*, char*);
 
 int	main(void)
 {
-	printf("expected 0 got %d\n", ft_strlen(""));
-	printf("expected 1 got %d\n", ft_strlen("a"));
-	printf("expected 3 got %d\n", ft_strlen("abc"));
+	/*
+	printf("  expected 0 got %d\n", ft_strlen(""));
+	printf("  expected 1 got %d\n", ft_strlen("a"));
+	printf("  expected 3 got %d\n", ft_strlen("abc"));
 	printf("expected 100 got %d\n", ft_strlen("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
+	*/
+	printf(" expected 0 got %d\n", ft_strcmp("", ""));
+	printf(" expected 0 got %d\n", ft_strcmp("aa", "aa"));
+	printf("expected -1 got %d\n", ft_strcmp("ax", "xa"));
+	printf(" expected 1 got %d\n", ft_strcmp("xa", "ax"));
+	printf(" expected 1 got %d\n", ft_strcmp("aaa", ""));
+	printf("expected -1 got %d\n", ft_strcmp("", "xaa"));
 }
