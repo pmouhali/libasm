e25ab010c4f77cddaffb1ef5b7e5f90b9bf0b280
pmouhali <p.m.272019@gmail.com>
2020-01-31 18:29:45 +0100

    ft_strcpy + dir for both linux et macos

---
+++ linuxver/ft_strcmp.s
@@ -0,0 +1,22 @@
+	global ft_strcmp
+	section .text
+
+ft_strcmp:
+	xor rax, rax
+	mov r8d, 1
+	mov r9d, -1
+
+loop:
+	cmp dil, sil
+	cmovg eax, r8d
+	cmovl eax, r9d
+
+	cmp dil, 0 
+	je end
+
+	inc rdi
+	inc rsi
+	jmp loop
+
+end:
+	ret
---
+++ linuxver/ft_strcpy.s
@@ -0,0 +1,18 @@
+	global ft_strcpy
+	section .text
+
+ft_strcpy:
+		xor rcx, rcx
+		lea rax, [rdi]				; rax contain now the *dest pointer
+
+loop:
+		mov dl, BYTE [rsi + rcx]	; using intermediary register bc copying
+		mov BYTE [rdi + rcx], dl	; from one string to another is impossible
+		cmp BYTE [rsi + rcx], 0
+		je end
+
+		inc rcx
+		jmp loop
+
+end:
+		ret
